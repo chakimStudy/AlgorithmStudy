cfec575770f3ec6f46c15a82c89ce42e51d442d5
ksy <love00826@naver.com>
2023-07-07 17:55:10 +0900

    code : solving

--- KSY/StudyProject/src/ps/ㄱSolving/BOJ_G3_17142_연구소3.java
+++ KSY/StudyProject/src/ps/ㄱSolving/BOJ_G3_17142_연구소3.java
@@ -1,4 +1,117 @@
 package ps.ㄱSolving;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class BOJ_G3_17142_연구소3 {
+    /*
+    조합 + bfs
+    조합 : 활성 바이러스 위치 선택
+    bfs : 바이러스 퍼트리기
+     */
+    static class Pos{
+        int r, c;
+        int depth;
+        public Pos(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+        public Pos(int r, int c, int depth){
+            this.r = r;
+            this.c = c;
+            this.depth = depth;
+        }
+
+    }
+    static int N, M;
+    static int[][] map;
+    static List<Pos> virus;
+    static int minTime = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        virus = new ArrayList<>();
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2){
+                    virus.add(new Pos(i, j));
+                }
+            }
+        }
+
+        //바이러스 위치 선택
+        int virusN = virus.size();
+        combRes = new int[virusN];
+        comb(0, 0, virusN); //조합: 뽑은 원소 수, 시작 인덱스
+
+        System.out.println(minTime);
+    }
+
+    static int[] combRes;
+    private static void comb(int cnt, int start, int size) {
+        if (cnt == M) {
+            //원소 다 뽑음 -> 바이러스 퍼트림
+            int[][] cloneMap = new int[N][N];
+            for (int i = 0; i < N; i++){
+                cloneMap[i] = map[i].clone();
+            }
+            for (int i : combRes){
+                map[virus.get(i).r][virus.get(i).c] = 3; //활성 바이러스
+            }
+            spread(cloneMap);
+        }
+
+        for (int i = start; i < size; i++){
+            combRes[i] = i;
+            comb(cnt+1, start+1, size);
+        }
+    }
+
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    private static void spread(int[][] cloneMap) {
+        Queue<Pos> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][N];
+
+        //활성 바이러스 위치를 큐에 넣음
+        for (int i : combRes){
+            q.offer(new Pos(virus.get(i).r, virus.get(i).c));
+        }
+
+        //bfs 수행
+        int lastDepth = 0;
+        while(!q.isEmpty()){
+            Pos cur = q.poll();
+            lastDepth = cur.depth;
+
+            for (int di = 0; di < 4; di++){
+                int nr = cur.r + dr[di];
+                int nc = cur.c + dc[di];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N || cloneMap[nr][nc] == '1' || visited[nr][nc]) continue;
+
+                //벽과 인덱스 넘어선 경우를 제외하고는 바이러스가 퍼짐
+                visited[nr][nc] = true;
+                cloneMap[nr][nc] = 3;
+                q.offer(new Pos(nr, nc, cur.depth + 1));
+            }
+        }
+
+
+        //다 차면 : 즉 이 없음
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                if (cloneMap[i][j] == 0) return;
+            }
+        }
+
+        minTime = Math.min(minTime, lastDepth);
+    }
 }
--- KSY/StudyProject/src/ps/ㄱSolving/BOJ_G4_17140_이차원배열과연산.java
+++ KSY/StudyProject/src/ps/ㄱSolving/BOJ_G4_17140_이차원배열과연산.java
@@ -1,81 +1,81 @@
-package ps.ㄱSolving;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-public class BOJ_G4_17140_이차원배열과연산 {
-    /*
-    처음에는 이차원 리스트를 쓰려고 하다가
-    행 정렬은 괜찮은데 열 정렬이 힘들 거 같아서 별로일 거 같음
-     */
-
-    static int[][] A;
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int r, c, k;
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        r = Integer.parseInt(st.nextToken()) - 1;
-        c = Integer.parseInt(st.nextToken()) - 1;
-        k = Integer.parseInt(st.nextToken());
-
-        //초기 배열 입력
-        A = new int[100][100];
-        for (int i = 0; i < 3; i++){
-            st = new StringTokenizer(in.readLine());
-            for (int j = 0; j < 3; j++){
-                A[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        boolean isOk = false;
-        int time = 1;
-        int rowL = 3, colL = 3;
-        for (time = 1; time <= 100; time++){
-            if (A[r][c] == k){
-                isOk = true;
-                break;
-            }
-
-            if (rowL >= colL){
-                //R연산 : 모든 행에 대해 정렬
-                rowL = sortR(colL);
-            }
-            else{
-                //C연산
-
-            }
-
-        }
-
-        if (isOk) System.out.println(time);
-        else System.out.println(-1);
-    }
-
-    private static int sortR(int colL) {
-        int maxRowL = 0;
-        for (int r = 0; r < colL; r++){
-            //각 row를 돌면서 정렬
-            int[][] nums = new int[101][2]; //1~100
-            for (int i = 0; i <= 100; i++){
-                nums[i][0] = i; //0: 숫자, 1: 카운트
-            }
-            for (int c = 0; c < colL; c++){
-                if (A[r][c] == 0) break;
-                nums[A[r][c]][1]++; //카운트 증가
-            }
-            //카운트로 오름차순 정렬
-            Arrays.sort(nums, new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o1[1] - o2[1];
-                }
-            });
-
-
-
-        }
-    }
-
-}
+//package ps.ㄱSolving;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.*;
+//
+//public class BOJ_G4_17140_이차원배열과연산 {
+//    /*
+//    처음에는 이차원 리스트를 쓰려고 하다가
+//    행 정렬은 괜찮은데 열 정렬이 힘들 거 같아서 별로일 거 같음
+//     */
+//
+//    static int[][] A;
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//        int r, c, k;
+//        StringTokenizer st = new StringTokenizer(in.readLine());
+//        r = Integer.parseInt(st.nextToken()) - 1;
+//        c = Integer.parseInt(st.nextToken()) - 1;
+//        k = Integer.parseInt(st.nextToken());
+//
+//        //초기 배열 입력
+//        A = new int[100][100];
+//        for (int i = 0; i < 3; i++){
+//            st = new StringTokenizer(in.readLine());
+//            for (int j = 0; j < 3; j++){
+//                A[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        boolean isOk = false;
+//        int time = 1;
+//        int rowL = 3, colL = 3;
+//        for (time = 1; time <= 100; time++){
+//            if (A[r][c] == k){
+//                isOk = true;
+//                break;
+//            }
+//
+//            if (rowL >= colL){
+//                //R연산 : 모든 행에 대해 정렬
+//                rowL = sortR(colL);
+//            }
+//            else{
+//                //C연산
+//
+//            }
+//
+//        }
+//
+//        if (isOk) System.out.println(time);
+//        else System.out.println(-1);
+//    }
+//
+//    private static int sortR(int colL) {
+//        int maxRowL = 0;
+//        for (int r = 0; r < colL; r++){
+//            //각 row를 돌면서 정렬
+//            int[][] nums = new int[101][2]; //1~100
+//            for (int i = 0; i <= 100; i++){
+//                nums[i][0] = i; //0: 숫자, 1: 카운트
+//            }
+//            for (int c = 0; c < colL; c++){
+//                if (A[r][c] == 0) break;
+//                nums[A[r][c]][1]++; //카운트 증가
+//            }
+//            //카운트로 오름차순 정렬
+//            Arrays.sort(nums, new Comparator<int[]>() {
+//                @Override
+//                public int compare(int[] o1, int[] o2) {
+//                    return o1[1] - o2[1];
+//                }
+//            });
+//
+//
+//
+//        }
+//    }
+//
+//}
