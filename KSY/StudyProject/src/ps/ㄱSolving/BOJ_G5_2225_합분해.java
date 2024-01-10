05864a7ca51ad8d86b879f2bc515574eac093be4
김승연 <love00826@naver.com>
2024-01-10 15:12:16 +0900

    :memo: code : boj G5 2225 합분해

---
+++ KSY/StudyProject/src/ps/ㄱSolving/BOJ_G5_2225_합분해.java
@@ -0,0 +1,34 @@
+package ps.ㄱSolving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G5_2225_합분해 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        //init
+        long[][] dp = new long[N+1][K+1];
+        for (int n = 0; n < N+1; n++){
+            dp[n][1] = 1;
+        }
+        for (int k = 0; k < K+1; k++){
+            dp[1][k] = k;
+        }
+
+        //logic
+        for (int n = 2; n < N+1; n++){
+            for (int k = 2; k < K+1; k++){
+                dp[n][k] = dp[n][k-1] + dp[n-1][k] % 1000000000;
+            }
+        }
+
+        System.out.println(dp[N][K] % 1000000000);
+    }
+}
