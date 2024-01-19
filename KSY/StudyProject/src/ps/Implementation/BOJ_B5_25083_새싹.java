c6d77527c46e0d8f9badb88dc7bf423bff15b70d
김승연 <love00826@naver.com>
2024-01-19 15:22:51 +0900

    :memo: BOJ B5 25083 새싹

---
+++ KSY/StudyProject/src/ps/Implementation/BOJ_B5_25083_새싹.java
@@ -0,0 +1,12 @@
+package ps.Implementation;
+
+public class BOJ_B5_25083_새싹 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
