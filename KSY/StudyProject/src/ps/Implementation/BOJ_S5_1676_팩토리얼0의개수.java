2f7c317649c04fa8456c58889ae3aa8ab37629ac
김승연 <love00826@naver.com>
2024-02-06 15:52:01 +0900

    :memo: BOJ S5 1676 팩토리얼0의개수

---
+++ KSY/StudyProject/src/ps/Implementation/BOJ_S5_1676_팩토리얼0의개수.java
@@ -0,0 +1,22 @@
+package ps.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_S5_1676_팩토리얼0의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int fiveCnt = 0;
+        for (int i = 1; i < N+1; i++){
+            int num = i;
+            while(num % 5 == 0){
+                num /= 5;
+                fiveCnt++;
+            }
+        }
+        System.out.println(fiveCnt);
+    }
+}
