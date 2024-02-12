3883db236451a6f4589e0f2de1ee5dbfc18ae57e
김승연 <love00826@naver.com>
2024-02-12 16:28:33 +0900

    :memo: BOJ S4 18110 solvedac

---
+++ KSY/StudyProject/src/ps/Implementation/BOJ_S4_18110_solvedac.java
@@ -0,0 +1,25 @@
+package ps.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_S4_18110_solvedac {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++){
+            nums[i] = Integer.parseInt(in.readLine());
+        }
+        Arrays.sort(nums);
+
+        int diff = (int) Math.round(N * 0.15);
+        int sum = 0;
+        for (int i = diff; i < N-diff; i++){
+            sum += nums[i];
+        }
+        System.out.println(Math.round(sum / (double)(N-diff*2)));
+    }
+}
