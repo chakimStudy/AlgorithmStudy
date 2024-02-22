e3a38d017b0577bfa9e7cc1ef867305cb886a85f
김승연 <love00826@naver.com>
2024-02-22 16:10:29 +0900

    :memo: BOJ S3 2606 바이러스

---
+++ KSY/StudyProject/src/ps/GraphSearch/BOJ_S3_2606_바이러스.java
@@ -0,0 +1,43 @@
+package ps.GraphSearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_S3_2606_바이러스 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int M = Integer.parseInt(in.readLine());
+        boolean[][] graph = new boolean[N+1][N+1];
+        while(M-- > 0){
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = true;
+            graph[b][a] = true;
+        }
+
+        boolean[] visited = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(1);
+        visited[1] = true;
+
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+
+            for (int adj = 2; adj < N+1; adj++){
+                if (!graph[cur][adj] || visited[adj]) continue;
+
+                visited[adj] = true;
+                q.offer(adj);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
