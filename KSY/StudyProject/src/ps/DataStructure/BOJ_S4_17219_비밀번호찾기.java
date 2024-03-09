13a1a847acc426b85d228817bae3730dbf0894b2
김승연 <love00826@naver.com>
2024-03-09 19:08:03 +0900

    :memo: BOJ S4 17219 비밀번호찾기

---
+++ KSY/StudyProject/src/ps/DataStructure/BOJ_S4_17219_비밀번호찾기.java
@@ -0,0 +1,28 @@
+package ps.DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ_S4_17219_비밀번호찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, String> save = new HashMap<>();
+        while(N-- > 0){
+            st = new StringTokenizer(in.readLine());
+            save.put(st.nextToken(), st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder();
+        while(M-- > 0){
+            sb.append(save.get(in.readLine())).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
