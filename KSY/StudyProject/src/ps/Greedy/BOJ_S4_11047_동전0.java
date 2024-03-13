0fc44d4a3df0e477c926c4aa391cc9a0a72906f9
김승연 <love00826@naver.com>
2024-03-13 14:02:12 +0900

    :memo: BOJ S4 11047 동전0

---
+++ KSY/StudyProject/src/ps/Greedy/BOJ_S4_11047_동전0.java
@@ -0,0 +1,27 @@
+package ps.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S4_11047_동전0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++){
+            coins[i] = Integer.parseInt(in.readLine());
+        }
+
+        int cnt = 0;
+        for (int i = N-1; i >= 0; i--){
+            cnt += (K / coins[i]);
+            K = K % coins[i];
+        }
+        System.out.println(cnt);
+    }
+}
