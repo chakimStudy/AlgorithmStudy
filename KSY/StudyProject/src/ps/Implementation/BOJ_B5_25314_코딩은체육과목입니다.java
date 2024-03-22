8ba9e60668c2d8d48998a5282a3697a567dba7a8
김승연 <love00826@naver.com>
2024-03-22 16:20:04 +0900

    :memo: BOJ B5 25314 코딩은체육과목입니다

---
+++ KSY/StudyProject/src/ps/Implementation/BOJ_B5_25314_코딩은체육과목입니다.java
@@ -0,0 +1,18 @@
+package ps.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_B5_25314_코딩은체육과목입니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N/4; i++){
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+    }
+}
